ff491c256fc7b81683399dd01152323341c0193f
Roxas0428 <daichan04280507@icloud.com>
2023-12-03 21:52:08 +0900

    リファクタリング後

--- db.js
+++ db.js
@@ -1,16 +1,3 @@
-// Sign In ボタンが押されたときのイベントハンドラ
-document
-  .querySelector(".login-button")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    var email = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    // emailとpasswordをlocalStorageに保存
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-  });
-
 // ページが読み込まれたときに、localStorageからemailとpasswordを取得して入力フィールドにセット
 window.onload = function () {
   var email = localStorage.getItem("email");
@@ -20,4 +7,17 @@ window.onload = function () {
     document.getElementById("username").value = email;
     document.getElementById("password").value = password;
   }
+
+  // Sign In ボタンが押されたときのイベントハンドラ
+  document
+    .querySelector(".login-button")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      var email = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+
+      // emailとpasswordをlocalStorageに保存
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
+    });
 };
